84d4abed33f8bff0718e6ae64c380386ddb7b9bb
caioreigot <caiocrrodrigues2004@gmail.com>
2022-10-30 00:12:34 -0300

    Metadados do Open Graph alterados

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <Fragment>
       <Head>
         <title>Caio Costa</title>
-        <meta name="description" content="Site e blog pessoal sobre tecnologia" />
         <link rel="icon" href="/c.svg" />
+        <meta name="description" content="Site e blog pessoal sobre tecnologia" />
+        <meta property="og:title" content="Caio Costa" />
+        <meta property="og:description" content="Site e blog pessoal sobre tecnologia" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://caio-costa.vercel.app/" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/62410044/198860813-fe4dbc85-5947-42ad-be3a-5375f1398a17.png" />
       </Head>
       <Component {...pageProps} />
     </Fragment>
