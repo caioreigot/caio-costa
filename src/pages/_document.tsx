914d6efce42afbed19b665320974df086d65ae66
BuildTools <unconfigured@null.spigotmc.org>
2023-07-19 20:51:20 -0300

    max-width da página alterado para 1920px

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className='h-full flex flex-row justify-center bg-blue-2 text-white text-xs md:text-sm lg:text-base'>
       <Head />
-      <body className='font-monospace flex flex-col overflow-y-auto custom-vertical-scrollbar h-full w-full max-w-[1800px]'>
+      <body className='font-monospace flex flex-col overflow-y-auto custom-vertical-scrollbar h-full w-full max-w-[1920px]'>
         <Main />
         <NextScript />
       </body>
